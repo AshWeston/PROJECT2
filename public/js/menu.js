4eae2707b0bc934e6db2ef34f3730d4e6db1a455
Alex <chaza.mckay@me.com>
2022-03-24 21:52:04 +1030

    Added nav bar animations

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,10 +1,4 @@
 // Top right hand menu functionality
-var userMenuDiv = document.getElementById("userMenu");
-var userMenuBtn = document.getElementById("userButton");
-
-var navMenuDiv = document.getElementById("nav-content");
-var navMenu = document.getElementById("nav-toggle");
-
 const logoutBtn = document.getElementById('logout');
 const homeBtn = document.getElementById('home');
 const dashboardBtn = document.getElementById('dashboard');
@@ -17,57 +11,6 @@ const homeA = document.querySelector('.home');
 const dashboardA = document.querySelector('.dashboard');
 const questionA = document.querySelector('.question');
 
-document.onclick = check;
-
-function check(e) {
-  var target = (e && e.target) || (event && event.srcElement);
-
-  //User Menu
-  if (!checkParent(target, userMenuDiv)) {
-    // click NOT on the menu
-    if (checkParent(target, userMenu)) {
-      // click on the link
-      if (userMenuDiv.classList.contains("invisible")) {
-        userMenuDiv.classList.remove("invisible");
-      } else {
-        userMenuDiv.classList.add("invisible");
-      }
-    } else {
-      // click both outside link and outside menu, hide menu
-      userMenuDiv.classList.add("invisible");
-    }
-  }
-
-  //Nav Menu
-  if (!checkParent(target, navMenuDiv)) {
-    // click NOT on the menu
-    if (checkParent(target, navMenu)) {
-      // click on the link
-      if (navMenuDiv.classList.contains("hidden")) {
-        navMenuDiv.classList.remove("hidden");
-      } else {
-        navMenuDiv.classList.add("hidden");
-      }
-    } else {
-      // click both outside link and outside menu, hide menu
-      navMenuDiv.classList.add("hidden");
-    }
-  }
-}
-
-function checkParent(t, elm) {
-  while (t.parentNode) {
-    if (t == elm) {
-      return true;
-    }
-    t = t.parentNode;
-  }
-  return false;
-}
-
-const logout = () => {
-
-}
 
 const home = () => {
   homeFav.classList.add('text-blue-400');
@@ -75,13 +18,13 @@ const home = () => {
   questionFav.classList.remove('text-blue-400');
   homeA.classList.replace('text-gray-500', 'text-blue-400');
   homeA.classList.replace('border-gray-900', 'border-blue-400');
-  homeA.classList.replace('hover:border-pink-400', 'hover:border-blue-400');
+  homeA.classList.replace('hover:border-red-400', 'hover:border-blue-400');
   dashboardA.classList.replace('text-blue-400', 'text-gray-500');
   dashboardA.classList.replace('border-blue-400', 'border-gray-900');
-  dashboardA.classList.replace('hover:border-blue-400', 'hover:border-pink-400');
+  dashboardA.classList.replace('hover:border-blue-400', 'hover:border-red-400');
   questionA.classList.replace('text-blue-400', 'text-gray-500');
   questionA.classList.replace('border-blue-400', 'border-gray-900');
-  questionA.classList.replace('hover:border-blue-400', 'hover:border-pink-400');
+  questionA.classList.replace('hover:border-blue-400', 'hover:border-red-400');
 }
 
 const dashboard = () => {
@@ -90,13 +33,13 @@ const dashboard = () => {
   questionFav.classList.remove('text-blue-400');
   dashboardA.classList.replace('text-gray-500', 'text-blue-400');
   dashboardA.classList.replace('border-gray-900', 'border-blue-400');
-  dashboardA.classList.replace('hover:border-pink-400', 'hover:border-blue-400');
+  dashboardA.classList.replace('hover:border-red-400', 'hover:border-blue-400');
   homeA.classList.replace('text-blue-400', 'text-gray-500');
   homeA.classList.replace('border-blue-400', 'border-gray-900');
-  homeA.classList.replace('hover:border-blue-400', 'hover:border-pink-400');
+  homeA.classList.replace('hover:border-blue-400', 'hover:border-red-400');
   questionA.classList.replace('text-blue-400', 'text-gray-500');
   questionA.classList.replace('border-blue-400', 'border-gray-900');
-  questionA.classList.replace('hover:border-blue-400', 'hover:border-pink-400');
+  questionA.classList.replace('hover:border-blue-400', 'hover:border-red-400');
 }
 
 const question = () => {
@@ -105,18 +48,16 @@ const question = () => {
   dashboardFav.classList.remove('text-blue-400');
   questionA.classList.replace('text-gray-500', 'text-blue-400');
   questionA.classList.replace('border-gray-900', 'border-blue-400');
-  questionA.classList.replace('hover:border-pink-400', 'hover:border-blue-400');
+  questionA.classList.replace('hover:border-red-400', 'hover:border-blue-400');
   dashboardA.classList.replace('text-blue-400', 'text-gray-500');
   dashboardA.classList.replace('border-blue-400', 'border-gray-900');
-  dashboardA.classList.replace('hover:border-blue-400', 'hover:border-pink-400');
+  dashboardA.classList.replace('hover:border-blue-400', 'hover:border-red-400');
   homeA.classList.replace('text-blue-400', 'text-gray-500');
   homeA.classList.replace('border-blue-400', 'border-gray-900');
-  homeA.classList.replace('hover:border-blue-400', 'hover:border-pink-400');
+  homeA.classList.replace('hover:border-blue-400', 'hover:border-red-400');
+
+
 }
-
-userMenuBtn.addEventListener('click', check)
-
-logoutBtn.addEventListener('click', logout)
 
 homeBtn.addEventListener('click', home)
 
