6590070d363a6c1cf4cfff8760c8dfa1f450a6c6
Ash Weston <ash-w@live.com.au>
2022-03-29 05:46:33 +1000

    add delete question functionality

--- controllers/api/questionRoutes.js
+++ controllers/api/questionRoutes.js
@@ -2,25 +2,39 @@ const router = require("express").Router();
 const { Question } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-router.post("/question", async (req, res) => {
+router.post("/question", withAuth, async (req, res) => {
   try {
     const newQuestion = await Question.create({
       ...req.body,
-      employee_id: req.session.user_id
+      employee_id: req.session.user_id,
     });
 
-    const questionData = await Question.findAll({
-
-    });
-    const question = questionData.map(el => el.get({
-      plain: true
-    }))
-   
+    const questionData = await Question.findAll({});
+    const question = questionData.map((el) =>
+      el.get({
+        plain: true,
+      })
+    );
 
     res.status(200).json(question);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(400).json(err);
+  }
+});
+
+router.delete("/question", withAuth, async (req, res) => {
+  try {
+    const deleteQuestion = await Question.destroy({
+      where: {
+        ...req.body,
+        employee_id: req.session.user_id,
+      },
+    });
+
+    res.status(200).json(deleteQuestion);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
--- public/js/question.js
+++ public/js/question.js
@@ -20,3 +20,29 @@ const questionHandler = async () => {
 document
   .querySelector("#question-form")
   .addEventListener("submit", questionHandler);
+
+async function deleteQuestionHandler(event) {
+  event.preventDefault();
+
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  const response = await fetch(`api/question`, {
+    method: "DELETE",
+    // body: JSON.stringify({question_title, question_contents}),
+    // headers: {
+    //   "Content-Type": "application/json",
+    // },
+  });
+
+  if (response.ok) {
+    document.location.replace("/question");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .querySelector("#deleteButton")
+  .addEventListener("click", deleteQuestionHandler);
